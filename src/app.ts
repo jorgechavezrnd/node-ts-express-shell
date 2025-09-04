e11c04fae2363f1841bd53646ce966bd05164482
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-04 16:41:13 -0400

    Add mongoose and configuration for connect to Mongo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
@@ -6,7 +7,12 @@ import { Server } from './presentation/server';
   main();
 })();
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
+
   const server = new Server({
     port: envs.PORT,
     publicPath: envs.PUBLIC_PATH,
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,5 +5,7 @@ export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
   PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+  MONGO_URL: get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
 
 };

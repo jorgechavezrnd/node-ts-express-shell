b96a3ecabb31cb0b2b55b86f9492c8bc20914df0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-06 19:36:08 -0400

    Add configuration for JSON Web Token and implementation for generate the jwt

---
+++ src/config/jwt.adapter.ts
@@ -0,0 +1,29 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
+
+export class JwtAdapter {
+
+  // DI?
+
+  static async generateToken(payload: any, duration: SignOptions["expiresIn"] = '2h') {
+
+    return new Promise((resolve) => {
+
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
+        if (err) return resolve(null);
+
+        resolve(token);
+
+      });
+
+    });
+
+  }
+
+  static validateToken(token: string) {
+    return;
+  }
+
+}
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { bcryptAdapter } from '../../config';
+import { bcryptAdapter, JwtAdapter } from '../../config';
 import { UserModel } from '../../data';
 import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from '../../domain';
 
@@ -47,9 +47,12 @@ export class AuthService {
 
     const { password, ...userEntity } = UserEntity.fromObject(user);
 
+    const token = await JwtAdapter.generateToken({ id: user.id, email: user.email });
+    if (!token) throw CustomError.internalServer('Error while creating JWT');
+
     return {
       user: userEntity,
-      token: 'ABC',
+      token: token,
     };
 
   }

29ba6ddfa0132c25fe6c04bf69252fbc0c1d1d3f
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-07 12:25:13 -0400

    Add create category DTO

---
+++ src/domain/dtos/category/create-category.dto.ts
@@ -0,0 +1,22 @@
+export class CreateCategoryDto {
+
+  private constructor(
+    public readonly name: string,
+    private readonly available: boolean,
+  ) {}
+
+  static create(object: { [key: string]: any } = {}): [undefined, CreateCategoryDto] | [string, undefined] {
+
+    const { name, available = false } = object;
+    let availableBoolean = available;
+
+    if (!name) return ['Missing name', undefined];
+    if (typeof available !== 'boolean') {
+      availableBoolean = (available === 'true')
+    }
+
+    return [undefined, new CreateCategoryDto(name, availableBoolean)];
+
+  }
+
+}

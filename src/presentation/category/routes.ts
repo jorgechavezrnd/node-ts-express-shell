727729f61a45bf77932a204d7d4351b68a8db70c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-07 12:51:11 -0400

    Add Auth middleware for create category endpoint

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
 
 export class CategoryRoutes {
 
@@ -10,7 +11,7 @@ export class CategoryRoutes {
 
     // Define routes
     router.get('/', controller.getCategories);
-    router.post('/', controller.createCategory);
+    router.post('/', [ AuthMiddleware.validateJWT ], controller.createCategory);
 
     return router;
 

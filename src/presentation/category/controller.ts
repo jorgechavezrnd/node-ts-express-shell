400ee8df685419cbc9e9a474e6c3cd2bffbd02d0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-07 14:13:46 -0400

    Add implementation for pagination of categories

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -35,11 +35,9 @@ export class CategoryController {
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
 
-    res.json(paginationDto);
-
-    // this.categoryService.getCategories()
-    //   .then(categories => res.json(categories))
-    //   .catch(error => this.handleError(error, res));
+    this.categoryService.getCategories(paginationDto!)
+      .then(categories => res.json(categories))
+      .catch(error => this.handleError(error, res));
 
   };
 

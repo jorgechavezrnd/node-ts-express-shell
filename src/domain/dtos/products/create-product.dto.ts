1b61b035b26190f74655bfe3fc2780dc424716d1
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-07 19:54:25 -0400

    Add create product DTO

---
+++ src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,41 @@
+export class CreateProductDto {
+
+  private constructor(
+    public readonly name: string,
+    public readonly available: boolean,
+    public readonly price: number,
+    public readonly description: string,
+    public readonly user: string, // ID
+    public readonly category: string, // ID
+  ) {}
+
+  static create(props: { [key: string]: any } = {}): [undefined, CreateProductDto] | [string, undefined] {
+
+    const {
+      name,
+      available,
+      price,
+      description,
+      user,
+      category,
+    } = props;
+
+    if (!name) return ['Missing name', undefined];
+    if (!user) return ['Missing user', undefined];
+    if (!category) return ['Missing category', undefined];
+
+    return [
+      undefined,
+      new CreateProductDto(
+        name,
+        !!available,
+        price,
+        description,
+        user,
+        category,
+      ),
+    ];
+
+  }
+
+}

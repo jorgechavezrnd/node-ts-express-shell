91c2626cce861ed7043d2a17e7a39f42bf41d23b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-25 20:06:57 -0400

    Add base structore of controller and routes for Upload Files

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+
+export class FileUploadRoutes {
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller = new FileUploadController();
+
+    // Define routes
+    router.post('/single/:type', controller.uploadFile);
+    router.post('/multiple/:type', controller.uploadMultipleFiles);
+
+    return router;
+
+  }
+
+}
